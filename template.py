4d011e58d62380eb8f8847240cf6080c6894dc1a
Hans <contact.kamik423@gmail.com>
2022-12-05 11:24:26 +0100

    added template

---
+++ template.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+import aoc
+
+
+def main(timer: aoc.Timer) -> None:
+    aoc.get_lines()
+
+
+if __name__ == "__main__":
+    with aoc.Timer() as timer:
+        main(timer)
